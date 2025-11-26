51769e59a0d2f4821b49628923695fc7e52d45c7
Satya <satyabratapadhiary6@gmail.com>
2025-11-26 10:25:19 +0530

    made the canvas smaller in width, added backdrop blur and a close button

--- frontend/src/components/Canvas.jsx
+++ frontend/src/components/Canvas.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react'
 import { Tldraw } from 'tldraw'
 import 'tldraw/tldraw.css'
 
 const  Canvas = ()=> {
+  const [canvasvisible,setcanvasvisible] = useState(true)
+  function togglecanvasvisibliity(){
+
+  }
 	return (
-		<div style={{ position: 'fixed', inset: 0 }}>
-			<Tldraw />
-		</div>
+		<div className="flex justify-center">
+      <button onClick={togglecanvasvisibliity} className="absolute top-0 z-10 bg-white w-30 h-10 cursor-pointer"></button>
+      <div className={`${canvasvisible? "flex":"hidden"} flex-col justify-center items-center fixed h-screen w-screen z-10 backdrop-blur-sm bg-black/30`}>
+        <div className="h-[90%] w-[70%] top-15 absolute flex flex-col gap-5 items-center ">
+          <button className="bg-red-200 cursor-pointer" onClick={togglecanvasvisibliity}>Close</button>
+          <Tldraw className='rounded-2xl'/>
+        </div>   
+      </div>
+    </div>
 	)
 }
 
