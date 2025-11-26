4a7381f80491a28d76b8162fb42dbb264e95cb6d
Devesh singh <deveshsingh173013@gmail.com>
2025-11-26 11:37:44 +0530

    Added hero component with title,get started button

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,59 @@
+import { useNavigate } from "react-router-dom";
+export default function Hero() {
+    const navigation = useNavigate()
+    return (
+        <section className="hero">
+
+            <div className="container hero-inner">
+                <h1 className="text-[58px] font-bold flex justify-center gap-5 cursor-pointer">
+                    <div className="flex">
+                        <span className="block transition-all hover:animate-rotate-once">D</span>
+                        <span className="block transition-all hover:animate-rotate-once">r</span>
+                        <span className="block transition-all hover:animate-rotate-once">a</span>
+                        <span className="block transition-all hover:animate-rotate-once">w</span>
+                    </div>
+                    <div className="flex">
+                        <span className="block transition-all hover:animate-rotate-once">I</span>
+                        <span className="block transition-all hover:animate-rotate-once">t</span>
+                    </div>
+                    <div className="flex text-violet-600">
+                        <span className="block transition-all hover:rotate-y-90">D</span>
+                        <span className="block transition-all hover:rotate-y-90">e</span>
+                        <span className="block transition-all hover:rotate-y-90">s</span>
+                        <span className="block transition-all hover:rotate-y-90">c</span>
+                        <span className="block transition-all hover:rotate-y-90">r</span>
+                        <span className="block transition-all hover:rotate-y-90">i</span>
+                        <span className="block transition-all hover:rotate-y-90">b</span>
+                        <span className="block transition-all hover:rotate-y-90">e</span>
+                    </div>
+                    <div className="flex text-violet-600">
+                        <span className="block transition-all hover:rotate-y-90">I</span>
+                        <span className="block transition-all hover:rotate-y-90">t</span>
+                    </div>
+                    <div className="flex">
+                        <span className="block transition-all hover:rotate-y-90">D</span>
+                        <span className="block transition-all hover:rotate-y-90">e</span>
+                        <span className="block transition-all hover:rotate-y-90">p</span>
+                        <span className="block transition-all hover:rotate-y-90">l</span>
+                        <span className="block transition-all hover:rotate-y-90">o</span>
+                        <span className="block transition-all hover:rotate-y-90">y</span>
+                    </div>
+                    <div className="flex">
+                        <span className="block transition-all hover:rotate-y-90">I</span>
+                        <span className="block transition-all hover:rotate-y-90">t</span>
+                    </div>
+                </h1>
+
+                <p className="hero-sub">
+                    Stop coding from scratch. Start morphing your ideas
+                </p>
+
+                <div className="hero-cta">
+                    <button onClick={() => { navigation('/app') }} className="bg-linear-to-br from-50% from-violet-700 to-violet-400 h-12 w-40 text-[16px] rounded-xl mt-8 font-bold cursor-pointer hover:bg-violet-500 hover:scale-110 transition-all">Get Started</button>
+                </div>
+
+                <p className="hero-note">You focus on ideas · Morphix builds the UI</p>
+            </div>
+        </section>
+    );
+}
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
+import Hero from "../components/Hero"
 import Navbar from "../components/Navbar"
 
 const Homepage = ()=>{
   return(
     <div id="homepagebody">
       <Navbar></Navbar>
+      <Hero></Hero>
     </div>
   )
 }
